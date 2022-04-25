7ef8ce3085e7b1f9c96cdccfc7ef4a280f21cfa6
Clare Hurst <Clare.Hurst@softwire.com>
2022-04-25 12:38:27 +0100

    Bus Board updates

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,49 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const node_fetch_1 = __importDefault(require("node-fetch"));
-(0, node_fetch_1.default)('https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals')
-    .then(res => res.text())
-    .then(body => console.log(body));
+function busTimes(stopID) {
+    (0, node_fetch_1.default)('https://api.tfl.gov.uk/StopPoint/' + stopID + '/Arrivals')
+        .then(res => res.json())
+        .then(body => processData(body));
+}
+function processData(data) {
+    const busList = new NextBuses();
+    for (const dataItem of data) {
+        let bus = new Bus(dataItem.id, dataItem.lineName, dataItem.destinationName, dataItem.timeToStation);
+        busList.addBus(bus);
+    }
+    busList.printNextBuses(5);
+}
+class NextBuses {
+    constructor() {
+        this.buses = [];
+    }
+    addBus(bus) {
+        this.buses.push(bus);
+    }
+    //SORTER
+    sortBuses() {
+        console.log("We will sort it");
+    }
+    printNextBuses(numBuses) {
+        for (let i = 0; i < numBuses; i++) {
+            this.buses[i].printBus();
+        }
+    }
+}
+class Bus {
+    constructor(busId, lineName, destinationName, timeToStation) {
+        this.busId = busId;
+        this.lineName = lineName;
+        this.destinationName = destinationName;
+        this.timeToStation = timeToStation;
+    }
+    printBus() {
+        console.log("Bus ID: " + this.busId);
+        console.log("Line Name: " + this.lineName);
+        console.log("Destination Name: " + this.destinationName);
+        console.log("Time to Station: " + this.timeToStation);
+    }
+}
+busTimes("490008660N");
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,59 @@
 import fetch from 'node-fetch';
 
-fetch('https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals')
-    .then(res => res.text())
-    .then(body => console.log(body));
+function busTimes(stopID: string) {
+    fetch('https://api.tfl.gov.uk/StopPoint/' + stopID + '/Arrivals')
+        .then(res => res.json())
+        .then(body => processData(body));
+}
+
+function processData(data: any) {
+    const busList = new NextBuses();
+    for (const dataItem of data) {
+        let bus = new Bus(dataItem.id, dataItem.lineName, dataItem.destinationName, dataItem.timeToStation);
+        busList.addBus(bus);
+    }
+    busList.printNextBuses(5);
+}
+
+class NextBuses {
+    buses: Bus[] = [];
+
+    addBus(bus: Bus): void {
+        this.buses.push(bus);
+    }
+
+    //SORTER
+    sortBuses(): void {
+        console.log("We will sort it");
+    }
+
+    printNextBuses(numBuses: number): void {
+        for (let i = 0; i < numBuses; i++) {
+            this.buses[i].printBus();
+        }
+    }
+}
+
+class Bus {
+    busId: string;
+    lineName: string;
+    destinationName: string;
+    timeToStation: number;
+
+    constructor(busId: string, lineName: string, destinationName: string, timeToStation: number) {
+        this.busId = busId;
+        this.lineName = lineName;
+        this.destinationName = destinationName;
+        this.timeToStation = timeToStation;
+    }
+
+    printBus(): void {
+        console.log("Bus ID: " + this.busId);
+        console.log("Line Name: " + this.lineName);
+        console.log("Destination Name: " + this.destinationName);
+        console.log("Time to Station: " + this.timeToStation);
+    }
+
+}
+
+busTimes("490008660N");
