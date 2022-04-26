95264271c86b607dbf4a07724acf75922b7f4fee
Clare Hurst <Clare.Hurst@softwire.com>
2022-04-26 14:09:15 +0100

    Added even better error handling

--- index.ts
+++ index.ts
@@ -2,14 +2,12 @@ import {busesFromPostcode} from "./functions/getBusInfoFunctions";
 import {getUserInput} from "./functions/userInput";
 
 async function printNextBusesNearPostcode() {
-    try {
-        const userInput = getUserInput();
-        await busesFromPostcode(userInput, 5, 2);
-    } catch (error: any) {
-        console.log("Caught error.");
-        console.log(error.message);
-    }
+    const userInput = getUserInput();
+    await busesFromPostcode(userInput, 5, 2);
 }
 
-printNextBusesNearPostcode().then(() => console.log("Finished running code."));
+printNextBusesNearPostcode().then(() => console.log("Finished running code.")).catch(function (error) {
+    console.log("Caught error.");
+    console.log(error.message);
+});
 
